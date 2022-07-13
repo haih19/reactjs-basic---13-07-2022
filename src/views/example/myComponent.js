974dc55cc438ec29dce92464fa58462de2e24901
haih19 <haih19c@gmail.com>
2022-07-13 18:45:30 +0700

    update video #5

--- src/views/example/myComponent.js
+++ src/views/example/myComponent.js
@@ -1,9 +1,41 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Mark',
+        age: 24
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleOnChangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+
+
     render() {
         return (
-            <div>Hello my component</div>
+            <>
+                <div className="information">
+                    <label htmlFor="">Enter your name:</label>
+                    <input onChange={this.handleOnChangeName} type="text" />
+                </div>
+                <div className="information">
+                    <label htmlFor="">Enter your age:</label>
+                    <input onChange={this.handleOnChangeAge} type="text" />
+                </div>
+                <div className="display">
+                    <div>My name's {this.state.name}</div>
+                    <div>My age's {this.state.age}</div>
+                </div>
+            </>
         )
     }
 }
